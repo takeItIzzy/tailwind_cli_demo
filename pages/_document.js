969a8852a02e31ac3031cdd53d13e4456c54b5cd
takeItIzzy <q1367257497@gmail.com>
2021-10-13 18:34:57 +0800

    add tailwindcss by tailwind cli

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    return await Document.getInitialProps(ctx);
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link href="tailwind.css" rel="stylesheet" />
+        </Head>
+        <body>
+        <Main />
+        <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
